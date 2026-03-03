f777b8c2f6cd286c85eb722369ca6de2c8a1d36b
Mohd Anas <mohdanas08022006@gmail.com>
2026-03-04 01:37:15 +0530

    import fixed

--- Db.js
+++ Db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-import userData from "./table";
+import userData from "./table.js";
 const dbUrl = process.env.DATABASE_URL || 'postgres://anas:Anas48@localhost:5432/loginsignup';
 
 const sequelize = new Sequelize(dbUrl, {
